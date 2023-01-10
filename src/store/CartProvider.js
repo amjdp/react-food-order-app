c5335bf9fcb7b9f6651d83fa520dbbd0c1455522
amjdp <athiramj123@gmail.com>
2023-01-10 12:23:10 +0530

    adding a cart reducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,15 +1,48 @@
+import { useReducer } from "react";
+
 import CartContext from "./CartContext";
 
-const CartProvider = props => {
-    const addItemToCartHandler = item => {};
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+}
 
-    const removeItemToCartHandler = item => {};
+const cartReducer = (state, action) => {
+    if(action.type === 'ADD') {
+        const updatedItems = state.items.concat(action.item);
+        // state.items - items in the current snapshot - first argument in the cartReducer
+        // concat() - adds a new item to an array but unlike push, it doesn't edit the existing array but return a new array
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        };
+    }
+    return defaultCartState;
+};
+
+const CartProvider = props => {
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+    const addItemToCartHandler = (item) => {
+        dispatchCartAction({
+            type: 'ADD',
+            item: item
+        });
+    };
+
+    const removeItemFromCartHandler = (id) => {
+        dispatchCartAction({
+            type: 'REMOVE',
+            id: id
+        });        
+    };
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
-        removeItem: removeItemToCartHandler
+        removeItem: removeItemFromCartHandler
     };
 
     return (
